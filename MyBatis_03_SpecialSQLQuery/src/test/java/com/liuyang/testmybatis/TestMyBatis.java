4205b89f35a79e7f151c4d3e20c82464e752006e
LIUYANG <yang.china.0201@gmail.com>
2023-02-15 14:13:01 +0800

    模糊查询

--- MyBatis_03_SpecialSQLQuery/src/test/java/com/liuyang/testmybatis/TestMyBatis.java
+++ MyBatis_03_SpecialSQLQuery/src/test/java/com/liuyang/testmybatis/TestMyBatis.java
@@ -6,6 +6,7 @@ import com.liuyang.utils.SqlSessionUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import java.sql.*;
 import java.util.List;
 
 public class TestMyBatis {
@@ -18,6 +19,44 @@ public class TestMyBatis {
         UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
         List<User> users = userMapper.fuzzyQuery("刘");
         users.forEach(System.out::println);
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void test_02_batchDelete(){
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        int result = userMapper.batchDelete("刘");
+        System.out.println("result = " + result);
+
+    }
+    
+    /**
+     * 
+     */
+    @Test
+    public void test_03_dynamicQueryTableName(){
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        List<User> users = userMapper.dynamicQueryTableName("t_user");
+        users.forEach(System.out::println);
+
+    }
+
+    /**
+     * 测试jdbc中获取主键
+     */
+    @Test
+    public void test_04_TestJdbc() throws ClassNotFoundException, SQLException {
+       Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mybatis_study?serverTimezone", "root", "Yang5211314..");
+        PreparedStatement preparedStatement = connection.prepareStatement("select * from t_user where id=8");
+        int i = preparedStatement.executeUpdate();
+        ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
+        System.out.println("generatedKeys = " + generatedKeys);
+
     }
     
 }
