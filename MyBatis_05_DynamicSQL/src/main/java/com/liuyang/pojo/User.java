33f603823f3aad779fd22ffdf5d11ab4c18d1cdc
LIUYANG <yang.china.0201@gmail.com>
2023-02-16 10:56:04 +0800

    .....

---
+++ MyBatis_05_DynamicSQL/src/main/java/com/liuyang/pojo/User.java
@@ -0,0 +1,123 @@
+package com.liuyang.pojo;
+
+public class User {
+    private Integer id;
+    private String userName;
+    private String password;
+    private Integer age;
+    private String gender;
+    private String email;
+
+
+    public User() {
+    }
+
+    public User(Integer id, String userName, String password, Integer age, String gender, String email) {
+        this.id = id;
+        this.userName = userName;
+        this.password = password;
+        this.age = age;
+        this.gender = gender;
+        this.email = email;
+    }
+
+    /**
+     * 获取
+     * @return id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * 设置
+     * @param id
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * 获取
+     * @return userName
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * 设置
+     * @param userName
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    /**
+     * 获取
+     * @return password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * 设置
+     * @param password
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * 获取
+     * @return age
+     */
+    public Integer getAge() {
+        return age;
+    }
+
+    /**
+     * 设置
+     * @param age
+     */
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    /**
+     * 获取
+     * @return gender
+     */
+    public String getGender() {
+        return gender;
+    }
+
+    /**
+     * 设置
+     * @param gender
+     */
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    /**
+     * 获取
+     * @return email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * 设置
+     * @param email
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String toString() {
+        return "User{id = " + id + ", userName = " + userName + ", password = " + password + ", age = " + age + ", gender = " + gender + ", email = " + email + "}";
+    }
+}
