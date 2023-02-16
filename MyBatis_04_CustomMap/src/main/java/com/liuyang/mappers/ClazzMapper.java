35d4d3436178d391641232acae37c8e38b421bbc
LIUYANG <yang.china.0201@gmail.com>
2023-02-16 10:55:29 +0800

    动态sql

--- MyBatis_04_CustomMap/src/main/java/com/liuyang/mappers/ClazzMapper.java
+++ MyBatis_04_CustomMap/src/main/java/com/liuyang/mappers/ClazzMapper.java
@@ -1,9 +1,16 @@
 package com.liuyang.mappers;
 
 import com.liuyang.pojo.Clazz;
+import org.apache.ibatis.annotations.Param;
 
 public interface ClazzMapper {
     Clazz queryClassById(Integer classId);
 
     Clazz howManyStudentsQueryInClass(Integer clazzId);
+    //分步查询
+
+    //通过id查询 班级
+    Clazz selectClazzById(@Param("clazzId") int clazzId);
+
+
 }
--- MyBatis_04_CustomMap/src/main/java/com/liuyang/mappers/StudentMapper.java
+++ MyBatis_04_CustomMap/src/main/java/com/liuyang/mappers/StudentMapper.java
@@ -3,6 +3,11 @@ package com.liuyang.mappers;
 import com.liuyang.pojo.Student;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+
 public interface StudentMapper {
     Student queryStudentTheClassById(@Param("id") int id);
+
+    List<Student> selectStudentByClazzId(@Param("clazzId") int clazzId);
+
 }
